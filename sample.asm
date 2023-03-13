36efec80642bbda3e35da9d9780fdc08a3de5b63
Harvs04 <hamartinez@up.edu.ph>
2023-03-13 09:55:32 +0800

    s

--- sample.asm
+++ sample.asm
@@ -6,10 +6,21 @@ section .data
     minutes dw 0
     seconds dd 0
 
+    mins db 60
+    secs db 60
+
 section .text
 _start:
-    
+    mov al, 0
+    mov al, byte[hours]
+    mul byte[mins]
+    mov word[minutes], ax
 
+    mov ax, 0
+    mov ax, word[minutes]
+    mul word[secs]
+    mov word[seconds], ax
+    mov word[seconds+2], dx
 
 exit_here:
     mov rax, 60
